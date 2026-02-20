e7845879806412de7b13e1f756dd52a60ac8cfdb
mickael <chezdijoux@gmail.com>
2026-02-20 21:18:56 +0100

    Dashboard admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('💀 La Petite Maison de l\'Épouvante')
+            ->setFaviconPath('favicon.ico');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Produits', 'fa fa-box', Product::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-users', User::class);
+        yield MenuItem::linkToRoute('Retour au site', 'fa fa-arrow-left', 'app_home');
+    }
+}
